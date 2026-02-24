66895e72509f8b3665b9d734f9b49ab123ee6c7d
devchalatse <chalatsethabo@gmail.com>
2026-02-25 00:26:11 +0200

    changes in my main program

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main(){
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("Get your tickets to attend")
+	
+}
