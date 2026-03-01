dddbd68749a51d96815c9962a21e9ade9f394e74
devchalatse <chalatsethabo@gmail.com>
2026-03-01 23:40:15 +0200

    changes in the main.go

--- main.go
+++ main.go
@@ -1,19 +1,32 @@
+// package main
+// import (
+// 	"fmt"
+// )
+
+// func sum(nums  ...int) int{
+// 	total := 0
+// 	for _, v := range nums{
+// 		total += v
+// 	}
+// 	return total
+// }
+
+// func main(){
+// 	results := sum(1, 2, 3, 4)
+// 	fmt.Println(results)
+// }
+
 package main
+
 import (
-	"fmt"
+    "errors"
+    "fmt"
 )
 
-func sum(nums  ...int) int{
-	total := 0
-	for _, v := range nums{
-		total += v
-	}
-	return total
-}
-
-func main(){
-	results := sum(1, 2, 3, 4)
-	fmt.Println(results)
-}
-
-
+func Greetings(name string) (string, error) {
+    if name == "" {
+        return "", errors.New("empty name")
+    }
+    message := fmt.Sprintf("Hi, %v! Welcome", name)
+    return message, nil
+}   
